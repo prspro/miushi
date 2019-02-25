9f8ba2db8f26348055a05b125af2b7cfd1a56359
prosp3ro <prospero1135@gmail.com>
2019-02-25 09:46:01 +0200

    +slider (no responsive), + lil fixes

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,16 @@
-//Some JS
+
+$(document).ready(function(){
+    $('.sale-carousel').slick({
+        infinite: true,
+        dots: true,
+        dotsClass: 'custom-dots',
+        arrows: true,
+        prevArrow: '<div class="arrow arrow--left"></div>',
+        nextArrow: '<div class="arrow arrow--right"></div>',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        slidesToShow: 1,
+        slidesToScroll: 1,  
+        //adaptiveHeight: true    
+    });
+});
