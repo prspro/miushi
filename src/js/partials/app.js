d88870c7a75f7cc3f527f5dbf84daba19be9b0ab
prosp3ro <prospero1135@gmail.com>
2019-02-26 23:07:37 +0200

    +wok (no responsive)

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -52,4 +52,17 @@ $(document).ready(function(){
         slidesToScroll: 1,  
         //adaptiveHeight: true    
     });
+    $('.wok-carousel').slick({
+        infinite: true,
+        dots: true,
+        dotsClass: 'custom-dots',
+        arrows: true,
+        prevArrow: '<div class="arrow arrow--left"></div>',
+        nextArrow: '<div class="arrow arrow--right"></div>',
+        autoplay: true,
+        autoplaySpeed: 4000,
+        slidesToShow: 4,
+        slidesToScroll: 1,  
+        //adaptiveHeight: true    
+    });
 });
