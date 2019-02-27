e15a8c2b3e18f28bd45b6593885fe0f94aaaf814
prosp3ro <prospero1135@gmail.com>
2019-02-27 17:50:29 +0200

    +new-stocks (not ready)

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -65,4 +65,33 @@ $(document).ready(function(){
         slidesToScroll: 1,  
         //adaptiveHeight: true    
     });
+    $('.new-stock-carousel').slick({
+        infinite: true,
+        dots: true,
+        dotsClass: 'custom-dots',
+        arrows: true,
+        prevArrow: '<div class="arrow arrow--left"></div>',
+        nextArrow: '<div class="arrow arrow--right"></div>',
+        autoplay: true,
+        autoplaySpeed: 4000,
+        slidesToShow: 4,
+        slidesToScroll: 1,  
+        //adaptiveHeight: true    
+        slickFilter: $('.slide--new')
+    });
+    
+    $('.btn--news').on('click', function(){
+        $('.new-stock-carousel').slick('slickUnfilter');   
+        $('.new-stock-carousel').slick('slickFilter', $('.slide--new'));
+        $('.btn--news')[0].classList.add('btn--active');
+        $('.btn--stock')[0].classList.remove('btn--active');
+    });
+    $('.btn--stock').on('click', function(){
+        $('.new-stock-carousel').slick('slickUnfilter');  
+        $('.new-stock-carousel').slick('slickFilter', $('.slide--stock'));
+        $('.btn--news')[0].classList.remove('btn--active');
+        $('.btn--stock')[0].classList.add('btn--active');
+    }); 
+    //$('.btn').css("border-color", "red");
+    //console.log($('.btn--news')[0].classList);
 });
